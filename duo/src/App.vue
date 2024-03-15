96b5f2e7bbde1416d1ce15a106854d3a843652b5
Sowhysos <z1240805012@163.com>
2024-03-15 17:05:33 +0800

    Initial commit

---
+++ duo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-link to="/home">三维城景</router-link>|
+  <router-link to="/blog">路线规划</router-link>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
